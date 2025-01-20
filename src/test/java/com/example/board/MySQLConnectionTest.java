af82ac9c68b33a823613321a22e6d87f2c10d275
juheonLee <jh.lee1@altimedia.com>
2025-01-20 17:51:47 +0900

    Added logback logger

--- src/test/java/com/example/board/MySQLConnectionTest.java
+++ src/test/java/com/example/board/MySQLConnectionTest.java
@@ -7,8 +7,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MySQLConnectionTest {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
     static final String DRIVER = "com.mysql.jdbc.Driver";
     static final String URL = "jdbc:mysql://127.0.0.1:3306/board?useSSL=false";
     static final String USERNAME = "board";
@@ -20,7 +24,7 @@ public class MySQLConnectionTest {
         Statement stmt = null;
         
         try {
-            System.out.println("==================== MySQL Connection START ====================");
+            logger.info("==================== MySQL Connection START ====================");
             
             Class.forName(DRIVER);
             
@@ -35,9 +39,10 @@ public class MySQLConnectionTest {
                 String boardContent = rs.getString("BOARD_CONTENT");
                 String boardWriter = rs.getString("BOARD_WRITER");
                 
-                System.out.print("boardSubject: " + boardSubject + ", ");
-                System.out.print("boardContent: " + boardContent + ", ");
-                System.out.println("boardWriter: " + boardWriter);
+                logger.info("boardSubject: {}", boardSubject);
+                logger.info("boardContent: {}", boardContent);
+                logger.info("boardWriter: {}", boardWriter);
+                logger.info("========================================");
             }
             
             rs.close();
@@ -63,6 +68,6 @@ public class MySQLConnectionTest {
                 se.printStackTrace();
             }
         }
-        System.out.println("==================== MySQL Connection END ====================");
+        logger.info("==================== MySQL Connection END ====================");
     }
 }
