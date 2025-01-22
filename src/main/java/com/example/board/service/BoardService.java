69fb3d0395a8c491ca61d1ea313a98476d34154c
boggydoggy <zhd401@naver.com>
2025-01-22 14:36:14 +0900

    Added Transaction about insert fail and Updated insDate and updDate

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -14,21 +14,21 @@ public class BoardService {
     @Autowired
     private BoardDao boardDao;
 
-    /* 게시판 - 목록 조회*/
+    /* 게시판 - 목록 조회 */
     public List<BoardDto> getBoardList(BoardForm boardForm) throws Exception {
 
         return boardDao.getBoardList(boardForm);
     }
-    
+
     /* 게시판 - 상세 조회 */
     public BoardDto getBoardDetail(BoardForm boardForm) throws Exception {
         BoardDto boardDto = new BoardDto();
-        
+
         String searchType = boardForm.getSearch_type();
-        
+
         if ("S".equals(searchType)) {
             int updateCnt = boardDao.updateBoardHits(boardForm);
-            
+
             if (updateCnt > 0) {
                 boardDto = boardDao.getBoardDetail(boardForm);
             }
@@ -37,52 +37,55 @@ public class BoardService {
         }
         return boardDto;
     }
-    
+
     /* 게시판 - 등록 */
     public BoardDto insertBoard(BoardForm boardForm) throws Exception {
- 
+
         BoardDto boardDto = new BoardDto();
- 
-        int insertCnt = boardDao.insertBoard(boardForm);
- 
+
+        int insertCnt = 0;
+
+        insertCnt = boardDao.insertBoard(boardForm);
+//        insertCnt = boardDao.insertBoardFail(boardForm);
+
         if (insertCnt > 0) {
             boardDto.setResult("SUCCESS");
         } else {
             boardDto.setResult("FAIL");
         }
- 
+
         return boardDto;
     }
- 
+
     /* 게시판 - 삭제 */
     public BoardDto deleteBoard(BoardForm boardForm) throws Exception {
- 
+
         BoardDto boardDto = new BoardDto();
- 
+
         int deleteCnt = boardDao.deleteBoard(boardForm);
- 
+
         if (deleteCnt > 0) {
             boardDto.setResult("SUCCESS");
         } else {
             boardDto.setResult("FAIL");
         }
- 
+
         return boardDto;
     }
- 
+
     /* 게시판 - 수정 */
     public BoardDto updateBoard(BoardForm boardForm) throws Exception {
- 
+
         BoardDto boardDto = new BoardDto();
- 
+
         int updateCnt = boardDao.updateBoard(boardForm);
- 
+
         if (updateCnt > 0) {
             boardDto.setResult("SUCCESS");
         } else {
             boardDto.setResult("FAIL");
         }
- 
+
         return boardDto;
     }
 }
